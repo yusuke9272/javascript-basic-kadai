536e61698609b8c7377bae240b71b6781cf9fe9c
yusuke9272 <160245634+yusuke9272@users.noreply.github.com>
2024-05-06 12:12:45 +0900

    Add files via upload

---
+++ kadai_009/holiday.js
@@ -0,0 +1,69 @@
+holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"];
+
+// for文
+for (let i=0; i<=15; i++) {
+    console.log(holidays[i]);
+}
+
+// 改行
+console.log('');
+
+// switch文
+for (let i=0; i<=15; i++) {
+    switch (i) {
+        case 0:
+        console.log(holidays[i]);
+        break;
+        case 1:
+        console.log(holidays[i]);
+        break;
+        case 2:
+        console.log(holidays[i]);
+        break;
+        case 3:
+        console.log(holidays[i]);
+        break;
+        case 0:
+        console.log(holidays[i]);
+        break;
+        case 4:
+        console.log(holidays[i]);
+        break;
+        case 5:
+        console.log(holidays[i]);
+        break;
+        case 6:
+        console.log(holidays[i]);
+        break;
+        case 7:
+        console.log(holidays[i]);
+        break;
+        case 8:
+        console.log(holidays[i]);
+        break;
+        case 9:
+        console.log(holidays[i]);
+        break;
+        case 10:
+        console.log(holidays[i]);
+        break;
+        case 11:
+        console.log(holidays[i]);
+        break;
+        case 12:
+        console.log(holidays[i]);
+        break;
+        case 13:
+        console.log(holidays[i]);
+        break;
+        case 14:
+        console.log(holidays[i]);
+        break;
+        case 15:
+        console.log(holidays[i]);
+        break;
+        case 16:
+        console.log(holidays[i]);
+        break;
+    }
+}
